b195482a1f20bf09db5ead3f3632c5cad03787fa
Leonardo <leonardonpsilva@gmail.com>
2025-12-14 16:38:40 -0300

    Ajuste: ajustando variáveis de ambiente para o vercel

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-	production: import.meta.env['PROD'],
+	production: import.meta.env['PRODUCTION'],
 	supabaseUrl: import.meta.env['NG_APP_SUPABASE_URL'],
 	supabaseKey: import.meta.env['NG_APP_SUPABASE_KEY'],
 };
